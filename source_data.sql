99ae68ceef27ac19bb4958bbcf9d2ea49f450e50
vijaykumarkadam <kadamvijayrm@gmail.com>
2026-06-09 13:32:00 +0530

    Create source_data.sql
    source sql table preparation scripts

---
+++ source_data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE source_cars_data
+(   
+  Branch_ID VARCHAR(200),
+  Dealer_ID VARCHAR(200),
+  Model_ID VARCHAR(200),
+  Revenue BIGINT,
+  Units_Sold BIGINT,
+  Date_ID VARCHAR(200),
+  Day INT,
+  Month INT,
+  Year INT,
+  BranchName VARCHAR(1000),
+  DealerName VARCHAR(1000),
+  Product_Name VARCHAR(1000)
+)  
